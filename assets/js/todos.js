81ee42824bf29dd1c8a8278a5ffa337cccb9ea67
zorazky <oswaldo.vazquez@gmail.com>
2019-12-03 10:09:21 -0600

    basic setup

---
+++ assets/js/todos.js
@@ -0,0 +1,15 @@
+// Check Off Specific Todos ny clicking
+
+$("li").click(function() {
+
+    $(this).toggleClass("completed")
+    
+})
+
+//click on x to delete todo
+$("span").click(function(event) {
+    $(this).parent().fadeOut(500, function() {
+        $(this).remove();
+    });
+    event.stopPropagation();
+})
